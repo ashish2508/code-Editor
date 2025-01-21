282cc47efa404b6dabfe0a4a2f87221f2d7c7dc5
ashish2508 <jha250805@gmail.com>
2025-01-21 18:43:28 +0530

    working on ouptut panel as of now

--- src/app/(root)/_components/OuterPanel.tsx
+++ src/app/(root)/_components/OuterPanel.tsx
@@ -1,8 +1,13 @@
+"use client"
+
+import { useCodeEditorStore } from "@/store/useCodeEditorStore"
 
 function OuterPanel() {
+  useCodeEditorStore();
   return (
     <div>OuterPanel</div>
   )
 }
 
 export default OuterPanel
+
